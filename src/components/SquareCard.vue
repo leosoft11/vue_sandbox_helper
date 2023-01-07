028f290fd834ad174fc2df7ecc011f638ffb2f19
leo <leo@leos-MacBook-Air.local>
2023-01-07 23:36:40 +0400

    Добавил логики для сквера и сервис imex, ну и пофиксил баги

--- src/components/SquareCard.vue
+++ src/components/SquareCard.vue
@@ -1,48 +1,88 @@
 <template>
    <div class="block">
+        <div class="loader" :class="{block: loaders}"></div>
                 <div class="block_row-info"> 
                     <img class="img_logo" src="@/images/square.png">
+
                      <div class="row">
                         <p>Название Sandbox</p>     
-                        <input type="text" class="input">
+                        <input v-model="sandbox_name" type="text" class="input">
                      </div>  
                       
                      <div class="row">
                         <p>app_id</p>
-                        <input type="text" class="input">
+                        <input v-model="app_id" type="text" class="input">
                      </div>
+
                      <div class="row">
                         <p>app_secret</p>
-                        <input type="text" class="input">
+                        <input v-model="app_secret" type="text" class="input">
                      </div>
+
                      <div class="row">
                         <p>sandbox_app_id</p>
-                        <input type="text" class="input">
+                        <input v-model="sandbox_app_id" type="text" class="input">
                      </div>
+
                      <div class="row">
                         <p>sandbox_access_token</p>
-                        <input type="text" class="input">
+                        <input v-model="sandbox_access_token" type="text" class="input">
                      </div>
+
                      <div class="row">
                         <p>webhook_key</p>
-                        <input type="text" class="input">
+                        <input v-model="webhook_key" type="text" class="input">
                      </div>
+
                      <div class="row">
                         <p>webhook_v2_key</p>
-                        <input type="text" class="input">
+                        <input v-model="webhook_v2_key" type="text" class="input">
                      </div>
                 </div>
+
                 <div class="block_row-bottom">
-                    <button class="btn">Настроить</button>
+                    <button @click="squareSettings" class="btn">Настроить</button>
                 </div>    
             </div>
 </template>
 
 <script>
+
+import child from 'child_process';
+
 export default {
     data() {
         return {
-            sandbox_name: ''
+            sandbox_name: '',
+            app_id: '',
+            app_secret: '',
+            sandbox_app_id: '',
+            sandbox_access_token: '',
+            webhook_key: '',
+            webhook_v2_key: '',
+            loaders: true,
+            info: 'Ошибок не было, значит сэндбокс почти настроен, осталось подождать запуска контейнеров на площадке',
+        }
+    },
+
+    methods: {
+        squareSettings() {
+        if (this.sandbox_name && this.app_id && this.app_secret && this.sandbox_app_id && this.sandbox_access_token && this.webhook_key && this.webhook_v2_key) {
+
+            this.loaders = false;
+            const exec_proc = (coommand) => {
+                    const s_process = child.exec(coommand);
+                    s_process.stdout.on('close', (code) => {
+                        console.log(code);
+                        this.loaders = true;
+                        alert(`${this.info}`);
+                    })
+            }
+            exec_proc(`bash src/bash/square.sh ${this.sandbox_name} ${this.app_id} ${this.app_secret} ${this.sandbox_app_id} ${this.sandbox_access_token} ${this.webhook_key} ${this.webhook_v2_key}`);
+
+        } else{
+            alert('Необходимо заполнить все поля')
+            }
         }
     }
 
--- src/components/imexCard.vue
+++ src/components/imexCard.vue
@@ -1,20 +1,53 @@
 <template>
     <div class="block">
                 <div class="block_row-info"> 
-                    <img class="img_logo" src="@/images/imex.png" height="50px">
+                    <img class="img_logo" src="@/images/imex.png">
                     <div class="row">
                         <p>Название Sandbox</p>     
-                        <input type="text" class="input">
+                        <input v-model="sandboxName" type="text" class="input">
                      </div>  
                 </div>
                 <div class="block_row-bottom">
-                    <button class="btn">Настроить</button>
+                    <button @click="imexSettings" class="btn">Настроить</button>
                 </div>    
             </div>
 </template>
 
 <script>
+
+import child from 'child_process';
+
 export default {
+
+    data() {
+        return {
+            sandboxName: '',
+            loaders: true,
+            info: 'Ошибок не было, значит сэндбокс почти настроен, осталось подождать запуска контейнеров на площадке',
+        }
+    },
+
+    methods: {
+        imexSettings() {
+            
+            if (this.sandboxName) {
+                
+                this.loaders = false;
+                const exec_proc = (coommand) => {
+                    const s_process = child.exec(coommand);
+                    s_process.stdout.on('close', (code) => {
+                        console.log(code);
+                        this.loaders = true;
+                        alert(`${this.info}`);
+                    })
+                }
+            exec_proc(`bash src/bash/imex.sh ${this.sandboxName}`);
+            } else {
+                alert('Необходимо заполнить все поля')
+            }
+
+        }
+    }
 
 }
 </script>
